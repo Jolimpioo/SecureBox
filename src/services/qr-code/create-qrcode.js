387db9da372a6a6a31a01184ae163cddf775eeaf
Jolimpioo <jolimpiomeloj@gmail.com>
2025-08-25 23:14:41 -0300

    refactor(qrcode): adapta create-qrcode para commander e remove dependência de prompt

--- src/services/qr-code/create-qrcode.js
+++ src/services/qr-code/create-qrcode.js
@@ -1,9 +1,19 @@
-import { ask } from "../../utils/prompt-ask.js";
-import promptSchemaQRCode from "../../prompts-schema/schema-qrcode.js";
 import handle from "./handle-qrcode.js";
 
-async function createQRCode() {
-    const result = await ask(promptSchemaQRCode);
+async function createQRCode(options) {
+    if (!options.link) {
+        console.log(chalk.red.bold("Erro:"));
+        console.log(chalk.red("É necessário informar um link para gerar o QR Code\n"));
+        console.log("Exemplo:");
+        console.log("  $ securebox qrcode --link https://meusite.com --terminal\n");
+        process.exit(1);
+    }
+
+    const result = {
+        link: options.link,
+        type: options ? 2 : 1,
+    }
+
     await handle(null, result);
 }
 
