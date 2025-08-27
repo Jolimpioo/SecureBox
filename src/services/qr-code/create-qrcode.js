e88852d24103778f0c63c801537d7a5396d03994
Jolimpioo <jolimpiomeloj@gmail.com>
2025-08-27 15:43:02 -0300

    chore(qrcode): remove import chalk não utilizado em create-qrcode

--- src/services/qr-code/create-qrcode.js
+++ src/services/qr-code/create-qrcode.js
@@ -1,17 +1,16 @@
-import chalk from "chalk";
 import handle from "./handle-qrcode.js";
 
 async function createQRCode(options) {
-    if (!options.link) {
+  if (!options.link) {
     throw new Error("É necessário informar um link para gerar o QR Code. \nEx: --link https://meusite.com");
   }
 
-    const result = {
-        link: options.link,
-        type: options.terminal ? 2 : 1,
-    };
+  const result = {
+    link: options.link,
+    type: options.terminal ? 2 : 1,
+  };
 
-    await handle(null, result);
+  await handle(null, result);
 }
 
 export default createQRCode;
