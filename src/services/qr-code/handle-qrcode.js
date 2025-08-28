6dbfa3303deea2d972d20be540de153862c9e33f
Jolimpioo <jolimpiomeloj@gmail.com>
2025-08-28 15:42:12 -0300

    refactor(qrcode): ajuste feedback inicial

--- src/services/qr-code/handle-qrcode.js
+++ src/services/qr-code/handle-qrcode.js
@@ -1,21 +1,25 @@
 import qr from "qrcode-terminal";
-import chalk from "chalk";
-import { printQRCodeFeedback } from "./utils/feedback-qrcode.js";
+import { printQRCodeSuccess, printQRCodeError } from "./utils/feedback-qrcode.js";
 
 async function handle(err, result) {
     if (err) {
-        console.log(chalk.red.bold("Erro ao gerar QR Code"));
-        console.error(err);
+        printQRCodeError(err);
         return;
     };
 
     const isSmall = result.type === 2;
 
-    printQRCodeFeedback(result.link, isSmall); 
+    try {
+        printQRCodeSuccess(result.link, isSmall);
 
-    qr.generate(result.link, { small: isSmall }, (qrcode) => {
-        console.log(qrcode);
-    });
+        qr.generate(result.link, { small: isSmall }, (qrcode) => {
+            console.log(qrcode);
+        });
+
+    } catch (error) {
+
+    }
+
 }
 
 export default handle;
--- src/services/qr-code/utils/feedback-qrcode.js
+++ src/services/qr-code/utils/feedback-qrcode.js
@@ -1,7 +1,16 @@
 import chalk from "chalk";
 
-export function printQRCodeFeedback(link, isSmall) {
+export function printQRCodeSuccess(link, isSmall) {
     console.log(chalk.green.bold("\nQR Code gerado com sucesso!\n"));
     console.log(chalk.yellow("Link: ") + chalk.white(link));
     console.log(chalk.yellow("Modo: ") + (isSmall ? "Terminal (compacto)" : "Normal"));
+}
+
+export function printQRCodeError(error) {
+  console.log(chalk.red.bold("[ERRO] Falha ao gerar QR Code."));
+  if (error instanceof Error) {
+    console.error(chalk.red(error.message));
+  } else {
+    console.error(chalk.red(error));
+  }
 }
